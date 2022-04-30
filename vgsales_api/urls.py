286b2575c7611fa8cfa147042be0efef96aef660
raj3k <rajkosky@gmail.com>
2022-04-30 14:07:13 +0200

    GameViewSet registered in routers

--- vgsales_api/urls.py
+++ vgsales_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, GroupViewSet
+from .views import UserViewSet, GroupViewSet, GameViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
+router.register(r'games', GameViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
