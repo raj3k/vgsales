e3765ffb8c2bd779500f5fc36f2ac2fb59f0b6ac
raj3k <rajkosky@gmail.com>
2022-04-30 13:07:00 +0200

    model for Game created

--- vgsales_api/models.py
+++ vgsales_api/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Game(models.Model):
-    name = models.CharField(max_length=120)
-    platform = models.CharField(max_length=20)
-    year = models.IntegerField()
-    genre = models.CharField(max_length=50)
-    publisher = models.CharField(max_length=120)
-    na_sales = models.FloatField()
-    eu_sales = models.FloatField()
-    jp_sales = models.FloatField()
-    other_sales = models.FloatField()
-    global_sales = models.FloatField()
+    name = models.CharField(max_length=200)
+    platform = models.CharField(max_length=20, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    genre = models.CharField(max_length=50, blank=True, null=True)
+    publisher = models.CharField(max_length=200, blank=True, null=True)
+    na_sales = models.FloatField(blank=True, null=True)
+    eu_sales = models.FloatField(blank=True, null=True)
+    jp_sales = models.FloatField(blank=True, null=True)
+    other_sales = models.FloatField(blank=True, null=True)
+    global_sales = models.FloatField(blank=True, null=True)
 
     def __str__(self) -> str:
         return self.name
