e26e6c6190c47b48a34257929cc72dd2035a54b2
raj3k <rajkosky@gmail.com>
2022-04-30 14:11:44 +0200

    added pagination

--- vgsales/settings.py
+++ vgsales/settings.py
@@ -86,6 +86,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 40
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
