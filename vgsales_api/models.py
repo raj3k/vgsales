8e089135912f0d52fbf0422740e7ef457661cb59
raj3k <rajkosky@gmail.com>
2022-04-30 08:58:57 +0200

    model Game added to app

--- vgsales_api/models.py
+++ vgsales_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=120)
+    platform = models.CharField(max_length=20)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=120)
+    na_sales = models.FloatField()
+    eu_sales = models.FloatField()
+    jp_sales = models.FloatField()
+    other_sales = models.FloatField()
+    global_sales = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.name
+
