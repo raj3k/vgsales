e678a22bc1f6bf966aa219bd3b4018f4f428d837
raj3k <rajkosky@gmail.com>
2022-04-30 08:45:41 +0200

    viewsets registered with router class and added to urlpatterns

--- vgsales_api/urls.py
+++ vgsales_api/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewSet, GroupViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
