f98b6078ac783e92744e973f84fa6e770aebde26
raj3k <rajkosky@gmail.com>
2022-04-30 08:12:04 +0200

    postgres db added to settings

--- vgsales/settings.py
+++ vgsales/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'vgsales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vgsales_db',
+        'USER': 'maciej',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
